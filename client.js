5dd118b2f4b1467a090fcf6bfc467d8e04122840
Ross <ross@tests-MacBook-Pro.local>
2017-12-12 14:48:52 -0600

    0.2 done

--- client.js
+++ client.js
@@ -1,12 +1,39 @@
-var atticus = { name: "Atticus", employeeNumber: "2405", annualSalary: "47000", reviewRating: 3 };
-var jem = { name: "Jem", employeeNumber: "62347", annualSalary: "63500", reviewRating: 4 };
-var boo = { name: "Boo", employeeNumber: "11435", annualSalary: "54000", reviewRating: 3 };
-var scout = { name: "Scout", employeeNumber: "6243", annualSalary: "74750", reviewRating: 5 };
-var robert = { name: "Robert", employeeNumber: "26835", annualSalary: "66000", reviewRating: 1 };
-var mayella = { name: "Mayella", employeeNumber: "89068", annualSalary: "35000", reviewRating: 2 };
+//0.1 - classes
+//0.2 - function that accepts an employee
+//0.3 - calculate review Bomus
+//0.4 - employee #
+//0.5 - max salary
+//0.6 - max/min bonus
+//0.7 - return object requested
+//0.8 - review rating as a function
+//0.9 employee # check as method on employee classes
+//1.0 - repeat through each employee
+//1.1 - on DOM
+
+
+class Employee{
+  constructor(name, employeeNumber, annualSalary, reviewRating){
+    this.name = name;
+    this.employeeNumber = employeeNumber;
+    this.annualSalary = annualSalary;
+    this.reviewRating = reviewRating;
+  } // end constructor
+} // end Employee class
+var atticus = new Employee("Atticus", "2405", "47000", 3 );
+var jem = new Employee("Jem", "62347", "63500", 4 );
+var boo = new Employee( "Boo",  "11435", "54000", 3 );
+var scout = new Employee( "Scout", "6243", "74750", 5 );
+var robert = new Employee("Robert",  "26835", "66000", 1 );
+var mayella = new Employee("Mayella",  "89068", "35000", 2 );
 
 var employees = [ atticus, jem, boo, scout, robert, mayella ];
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
 console.log(employees);
+
+function calculateEmployee(employee){
+  console.log('in calculateEmployee', employee);
+} //end calculateEmployee
+
+calculateEmployee(scout);
